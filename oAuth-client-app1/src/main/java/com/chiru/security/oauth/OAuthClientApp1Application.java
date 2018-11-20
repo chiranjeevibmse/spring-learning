2e945088e444c0f235d7b72837f34f6fe627dfb6
Chiranjeevi GK <chiranjeevibmse@gmail.com>
2018-11-20 17:51:14 +0530

    added oAuth with Facebook

--- oAuth-client-app1/src/main/java/com/chiru/security/oauth/OAuthClientApp1Application.java
+++ oAuth-client-app1/src/main/java/com/chiru/security/oauth/OAuthClientApp1Application.java
@@ -2,10 +2,10 @@ package com.chiru.security.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@EnableWebSecurity
+@EnableOAuth2Sso
 public class OAuthClientApp1Application {
 
 	public static void main(String[] args) {
